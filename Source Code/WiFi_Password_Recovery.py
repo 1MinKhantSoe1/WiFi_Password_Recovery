f8d0b65c9d6ae8ec73caf70748d71ca7c7632cb1
Min Khant Soe <63970830+1MinKhantSoe1@users.noreply.github.com>
2021-07-31 02:46:20 +0800

    Add files via upload

---
+++ Source Code/WiFi_Password_Recovery.py
@@ -0,0 +1,33 @@
+import subprocess
+
+
+def main():
+
+    print("WiFi Password Recovery")
+    print("----------------------")
+
+    print("")
+
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+    print("    Wifi Name \t\t\t Password ")
+    print("-" * 20, "\t\t-----------------")
+    for i in profiles:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split(
+            '\n')
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            print("{:<30}|  {:<}".format(i, results[0]))
+        except IndexError:
+            print("{:<30}|  {:<}".format(i, ""))
+
+    print("")
+
+    print("Created By Min Khant Soe (HakHak)")
+
+    input("")
+
+
+if __name__ == '__main__':
+    main()
